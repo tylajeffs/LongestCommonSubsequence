9a30b1068e10503e86773b2228b6db5f6f6b57dc
tylajeffs <tylajeffs@gmail.com>
2022-03-24 18:13:55 -1000

    fixed all the bugs!

--- LongestCommonSubsequence/LongestCommonSubsequence.cpp
+++ LongestCommonSubsequence/LongestCommonSubsequence.cpp
@@ -16,14 +16,16 @@ vector<string> lines;
 int numLines;
 
 //create two tables for the numbers and the arrows
-vector<vector<(m + 1)>> arrows;
-vector<vector<(n + 1)>> numbers;
+//vector<vector<string>> arrows(m + 1);
+//vector<vector<int>> numbers(n + 1);
 
+string arrows[30][30];
+int numbers[30][30];
 
 
 //methods
 void lcsLength(string x, string y);
-void printLcs();
+void printLCS(string x, int i, int j);
 
 
 
@@ -60,11 +62,20 @@ int main(int argc, char** argv)
 
 
 
+    //print the strings
+    cout << "String X: " << firstString << endl;
+    cout << "String Y: " << secondString << endl;
+
+
+
+
     //call lcs length to construct tables
-    lcsLength(x, y);
+    lcsLength(firstString, secondString);
 
     //print the lcs
-    printLCS(x, x.length(), y.length());
+    cout << "LCS: ";
+
+    printLCS(firstString, firstString.length(), secondString.length());
 
 
 
@@ -87,42 +98,42 @@ void lcsLength(string x, string y)
     //make the first row and column filled with 0's
     for (int i = 1; i <= m; i++)
     {
-        numbers[i, 0] = 0;
+        numbers[i][0] = 0;
     }
     for (int i = 0; i <= n; i++)
     {
-        numbers[0, i] = 0;
+        numbers[0][i] = 0;
     }
 
 
     //
     for (int i = 1; i <= m; i++)
     {
-        for (int j = 1; j <= n)
+        for (int j = 1; j <= n; j++)
         {
-            if (x.[i-1] == y[j-1])
+            if (x[i-1] == y[j-1])
             {
                 //change the number
-                numbers[i, j] = (numbers[(i - 1), (j - 1)] + 1);
+                numbers[i][j] = (numbers[i - 1][j - 1] + 1);
 
                 //assign the arrow
-                arrows[i, j] = "diagonal (up-left)";
+                arrows[i][j] = "diagonal (up-left)";
             }
-            else if (numbers[i - 1, j] >= numbers[i, j - 1])
+            else if (numbers[i - 1][j] >= numbers[i][j - 1])
             {
                 //set the number
-                numbers[i, j] = numbers[(i - 1),j];
+                numbers[i][j] = numbers[i - 1][j];
 
                 //assign the arrow
-                arrows[i, j] = "up";   
+                arrows[i][j] = "up";   
             }
             else
             {
                 //set the number
-                numbers[i, j] = numbers[i, (j - 1)];
+                numbers[i][j] = numbers[i][j - 1];
 
                 //assign the arrow
-                arrows[i, j] = "left";
+                arrows[i][j] = "left";
             }
 
         }
@@ -137,20 +148,20 @@ void printLCS(string x, int i, int j)
 {
     if (i == 0 or j == 0)
     {
-        return
+        return;
     }
 
-    if (arrows[i, j] == "diagonal (up-left)")
+    if (arrows[i][j] == "diagonal (up-left)")
     {
         printLCS(x, i - 1, j - 1);
 
         //print it
         cout << x[i - 1];
     }
-    else if (arrows[i, j] == "up")
+    else if (arrows[i][j] == "up")
     {
         //print
-        printLCS(x,i-1,j)
+        printLCS(x, i - 1, j);
 
     }
     else
